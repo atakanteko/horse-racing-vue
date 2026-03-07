d25eef9b5a77edd310111985938f4ce6d69b10fd
atakantekoglu <atakantekoglu@gmail.com>
2026-03-07 04:34:36 +0300

    remove bg on racing and boards components

--- src/components/racing/RacingBoard.vue
+++ src/components/racing/RacingBoard.vue
@@ -3,8 +3,7 @@
 </script>
 
 <template>
-  <div class="bg-white rounded-lg p-4 shadow-sm h-full">
+  <div class="rounded-lg p-4 shadow-sm h-full">
     <h2 class="text-lg font-semibold mb-4">Board</h2>
-
   </div>
 </template>
